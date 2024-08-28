a1db1cf5cca9365ad24bacf81eae5fcb6a248195
Jakub Niewelt <jakubniewelt@icloud.com>
2024-08-28 22:24:04 +0200

    docs: bump ilias version to 8

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 $id = 'sragpegasushelper';
 $version = '3.0.0';
-$ilias_min_version = '5.3.0';
-$ilias_max_version = '7.99';
+$ilias_min_version = '7.0.0';
+$ilias_max_version = '8.99';
 $responsible = 'fluxlabs AG';
 $responsible_mail = 'support@fluxlabs.ch';
